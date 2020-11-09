44f7b5bd130a184c4e7295163ef8a22038b03a01
nakamurataiti <67047092+nakamurataiti@users.noreply.github.com>
2020-11-09 21:51:32 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,64 @@
+class PrototypesController < ApplicationController
+  before_action :set_tweet, only: [:edit, :show]
+  before_action :move_to_edit, except: [:index, :show, ]
+
+
+  def index
+    @prototypes = Prototype.all
+  end
+
+  def new
+    @prototype = Prototype.new
+  end
+  
+  def create
+    @prototype = Prototype.new(prototype_params)
+    if @prototype.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+  
+  def show
+    @comment = Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+  
+  def edit
+  end
+  
+  def update
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path
+    else
+      render :edit
+    end
+  end
+
+
+  def destroy
+    @prototype = Prototype.find(params[:id])
+    @prototype.destroy
+    redirect_to root_path
+  end
+
+
+  private
+
+  def prototype_params
+    params.require(:prototype).permit(:title, :image, :catch_copy, :concept).merge(user_id: current_user.id )
+  end
+
+  def set_tweet
+    @prototype = Prototype.find(params[:id])
+  end
+
+  def move_to_edit
+    @prototype = Prototype.find(params[:id])
+    unless user_signed_in? && current_user.id == @prototype.user_id
+      redirect_to  action: :index
+    end
+  end
+end
